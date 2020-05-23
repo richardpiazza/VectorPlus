b014b47791dd47c348de0a9575d6f71a1b4d690e
Richard Piazza <richard@richardpiazza.com>
2020-05-23 11:49:45 -0500

    Updated Swift2D version to 0.1.1

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/richardpiazza/Swift2D.git",
-            from: "0.1.0"
+            from: "0.1.1"
         ),
         .package(
             url: "https://github.com/richardpiazza/SwiftColor.git",
